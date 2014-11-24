3ff2a73fa6e6e2f2499189842d5619991cf19cd0
Hari <hari.bageski@gmail.com>
2014-11-24 03:34:13 +0100

    all in one script

--- plot1.R
+++ plot1.R
@@ -43,14 +43,64 @@ Total_Emission_Baltimore <- sqldf("select year,type,sum(Emissions) as TotalEmiss
                        from NEI 
                        where fips == 24510
                        group by year,type")
+#where 24510 is the code for Baltimore City
 qplot(year,TotalEmission, data= Total_Emission_Baltimore,color=type)
 
 
 #4. Across the United States, how have emissions from coal combustion-related 
 #sources changed from 1999-2008?
-Total_Emission_Coal <- sqldf("select year,SCC as num_name,sum(Emissions) as TotalEmission
-                       from NEI 
-                       where num_name in SCC$
-                       group by year")
+colnames(SCC)[4]<- "Sector" #the initial name EI.Sector produces error in SQL
+SCC_COAL_COMBUST_RELATED <- sqldf("select SCC from SCC 
+                                  where Sector like '%Coal%'
+                                  and Sector like '%Comb%'")
 
-with(Total_Emission_Coal, plot(year,TotalEmission))
+Total_Emission_Coal <- sqldf("select year,sum(Emissions) as TotalEmission
+                            from NEI 
+                            where SCC in SCC_COAL_COMBUST_RELATED
+                            group by year")
+qplot(year,TotalEmission, data= Total_Emission_Coal)
+#We can see a drastic decrease from 2005 to 2008
+
+
+#5. How have emissions from motor vehicle sources changed from 1999-2008
+#in Baltimore City?
+SCC_motor_vhc <- sqldf("select SCC from SCC 
+                                  where Sector like '%Vehicle%'")
+
+Total_Emission_Vehic_Balt <- sqldf("select year,sum(Emissions) as TotalEmission
+                            from NEI 
+                            where SCC in SCC_motor_vhc
+                            and fips == '24510'
+                            group by year")
+qplot(year,TotalEmission, data= Total_Emission_Vehic_Balt)
+#The graph shows biggest decrease in 1999 and decrease in overall
+
+
+#Compare emissions from motor vehicle sources in Baltimore City with 
+#emissions from motor vehicle sources in Los Angeles County, 
+#California (fips == "06037"). Which city has seen greater changes over time 
+#in motor vehicle emissions?
+
+j<-2:4
+    Emission_change_Baltimore = -Total_Emission_Vehic_Balt[j-1,]$TotalEmission+
+      Total_Emission_Vehic_Balt[j,]$TotalEmission
+
+Emission_change_Baltimore=sum(Emission_change_Baltimore)
+Emission_change_Baltimore
+#That is the emition decrease for Baltimore.
+
+
+#We do the same for LA (fips == "06037")
+Total_Emission_Vehic_LA <- sqldf("select year,sum(Emissions) as TotalEmission
+                            from NEI 
+                            where  fips == '06037'
+                            and SCC in SCC_motor_vhc
+                            group by year")
+
+
+Emission_change_LA = -Total_Emission_Vehic_LA[j-1,]$TotalEmission + 
+  Total_Emission_Vehic_LA[j,]$TotalEmission
+
+Emission_change_LA=sum(Emission_change_LA)
+Emission_change_LA
+#We get that there is an incease in overall emission in LA.
